720d3ba0221b7e4af4ca7c55dbacc8924a40820a
TetianaKhomiak <tkhomiak8889@gmail.com>
2024-03-19 20:35:39 +0100

    feat: routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Cart from "./pages/Cart.jsx";
+import Menu from "./pages/Menu.jsx";
+import Login from "./pages/Login.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/pizzas-app/",
+    element: <App />,
+    children: [
+      {
+        path: "/pizzas-app/",
+        element: <Login />,
+      },
+      {
+        path: "/pizzas-app/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/pizzas-app/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
